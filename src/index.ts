bbf4b8538c78ad9835b47c7210b921e1280d5f18
Alberto Benatti <albi.benni8@gmail.com>
2023-11-12 18:14:25 +0100

    test cwt and dirname

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import { dirName, logCwt } from "./node-functions/cwd";
 import { str } from "./type-test/infer";
 
-const test = () => {
-  console.log("Es running ");
-};
+// const test = () => {
+//   console.log("Es running ");
+// };
 
-test();
-console.log(str());
+// test();
+// console.log(str());
+
+logCwt();
+dirName();
---
+++ src/node-functions/cwd.ts
@@ -0,0 +1,7 @@
+export const logCwt = () => {
+  console.log(process.cwd());
+};
+
+export const dirName = () => {
+  console.log(__dirname);
+};
