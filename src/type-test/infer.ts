1685874907db3cd8763d27907e15691f8697ec72
Alberto Benatti <albi.benni8@gmail.com>
2023-11-12 11:30:33 +0100

    infer

--- src/type-test/infer.ts
+++ src/type-test/infer.ts
@@ -6,17 +6,32 @@ export const func = (check: boolean) => {
   return "123123123";
 };
 
-type FuncResult = ReturnType<typeof func>;
-
 // type ReturnType<T extends (...args: any) => any> = T extends (
 //   ...args: any
 // ) => infer R
 //   ? R
 //   : any;
 
-// type Result2 = typeof func extends (...args: any) => infer R ? R : any;
-type Result2 = typeof func extends (...args: any) => any ? 1 : 0;
-
 //String Example
 export const str = () =>
   `hello_world-friend`.replace(/(_|-)/g, (item) => `${item}${item}${item}`);
+
+type Result2 = typeof func extends (...args: any) => infer R ? R : 0; // R is the inferred type
+
+const notMatching = {};
+
+type Result3 = typeof notMatching extends (...args: any) => infer R ? R : never; // R is the inferred type
+
+type FakeReturnType<T> = T extends (...args: any) => infer R ? R : never;
+
+type FuncResult = FakeReturnType<typeof notMatching>;
+
+const myString = () => {
+  return "hello_world-friend" as const;
+};
+
+type FakeReturnType2<T> = T extends ((...args: any) => infer R extends string)
+  ? `${R}_return_type`
+  : never;
+
+type FuncResult2 = FakeReturnType2<typeof myString>;
