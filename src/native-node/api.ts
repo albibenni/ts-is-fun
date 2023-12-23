baad61c13c1d8c88b77eed65e097b9ae105d994d
Alberto Benatti <albi.benni8@gmail.com>
2023-12-23 17:31:26 +0100

    basic node server

---
+++ src/native-node/api.ts
@@ -0,0 +1,50 @@
+import http from "http";
+import path from "path";
+import fs from "fs";
+
+const PORT = 8000;
+
+const MIME_TYPES = {
+  default: "application/octet-stream",
+  html: "text/html; charset=UTF-8",
+  js: "application/javascript",
+  css: "text/css",
+  png: "image/png",
+  jpg: "image/jpg",
+  gif: "image/gif",
+  ico: "image/x-icon",
+  svg: "image/svg+xml",
+} as const;
+
+type MimeType = keyof typeof MIME_TYPES;
+
+const STATIC_PATH = path.join(process.cwd(), "./static");
+
+const toBool = [() => true, () => false];
+
+const prepareFile = async (url: string) => {
+  const paths = [STATIC_PATH, url];
+  if (url.endsWith("/")) paths.push("index.html");
+  const filePath = path.join(...paths);
+  const pathTraversal = !filePath.startsWith(STATIC_PATH);
+  const exists = await fs.promises.access(filePath).then(...toBool);
+  const found = !pathTraversal && exists;
+  const streamPath = found ? filePath : STATIC_PATH + "/404.html";
+  const ext = path.extname(streamPath).substring(1).toLowerCase();
+  const stream = fs.createReadStream(streamPath);
+  return { found, ext, stream };
+};
+
+http
+  .createServer(async (req, res) => {
+    if (!req || !req.url) return;
+    const file = await prepareFile(req.url);
+    const statusCode = file.found ? 200 : 404;
+    const mimeType = MIME_TYPES[file.ext as MimeType] || MIME_TYPES.default;
+    res.writeHead(statusCode, { "Content-Type": mimeType });
+    file.stream.pipe(res);
+    console.log(`${req.method} ${req.url} ${statusCode}`);
+  })
+  .listen(PORT);
+
+console.log(`Server running at http://localhost:${PORT}/`);
