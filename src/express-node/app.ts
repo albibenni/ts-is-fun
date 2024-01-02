1f120362b53bd0fc06698a93f236dd6d3f227431
Alberto Benatti <albi.benni8@gmail.com>
2024-01-02 12:08:17 +0100

    fix some config to use commonjs and ts-node

--- src/express-node/app.ts
+++ src/express-node/app.ts
@@ -1,1 +1,27 @@
-console.log("Hello Express Node!");
+import express from "express";
+
+// Create an instance of Express
+const app = express();
+
+// Define a route for the root path
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+// Define a route for /about path
+app.get("/about", (req, res) => {
+  res.send("About us page");
+});
+
+// Define a route for handling 404 errors
+app.use((req, res) => {
+  res.status(404).send("Page not found");
+});
+
+// Set the port for the server to listen on
+const port = 3000;
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
