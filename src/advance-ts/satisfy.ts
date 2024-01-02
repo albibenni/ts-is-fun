6b5a5123d9766547589b7e61e04168930cd0a882
Alberto Benatti <albi.benni8@gmail.com>
2024-01-02 16:03:05 +0100

    config with satisfy

--- src/advance-ts/satisfy.ts
+++ src/advance-ts/satisfy.ts
@@ -1,4 +1,5 @@
-const scores: Record<string, number> = {};
+// Example 1 - incorrect use of satisfy
+const scores: Record<string, number> = {}; // record usage used as example
 scores.english = 100;
 scores.maths = 100;
 
@@ -6,3 +7,13 @@ scores.maths = 100;
 const scoresIncorrectUseOfSatisfy = {} satisfies Record<string, number>;
 // scoresIncorrectUseOfSatisfy.english = 100;   //* Property 'english' does not exist on type '{}'.
 // scoresIncorrectUseOfSatisfy.maths = 100;     //* Property 'maths' does not exist on type '{}'.
+
+// Example 2
+const config = {
+  wide: "100px",
+  narrow: 0,
+} satisfies Record<string, string | number>;
+config.wide;
+//      ^?
+config.narrow;
+//      ^?
