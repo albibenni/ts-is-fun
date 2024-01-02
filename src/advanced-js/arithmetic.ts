c9816f9d77a5dbb7698f8b889cd523b21a285fd2
Alberto Benatti <albi.benni8@gmail.com>
2024-01-02 21:51:02 +0100

    all math

--- src/advanced-js/arithmetic.ts
+++ src/advanced-js/arithmetic.ts
@@ -9,6 +9,8 @@ log(
   myExponentiation2,
 );
 
+log("exponentiation - cube root: ", Math.pow(9, 1 / 3));
+
 const ceiling = Math.ceil(1.4);
 const floor = Math.floor(1.4);
 const round = Math.round(1.4);
@@ -19,3 +21,6 @@ const random = Math.random();
 log("Random number: ", random);
 const myRandom = Math.floor(Math.random() * 10);
 log("Random number between 0 and 10: ", myRandom);
+
+const sign = Math.sign(-10);
+log("Sign: ", sign);
