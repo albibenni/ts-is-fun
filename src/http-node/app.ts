0493e1a587b1843e94332cd6b184d647b644286d
Alberto Benatti <albi.benni8@gmail.com>
2024-01-04 11:08:06 +0100

    wow stuff with write response

--- src/http-node/app.ts
+++ src/http-node/app.ts
@@ -1,12 +1,23 @@
+import { log } from "console";
 import http from "http";
 
 const requestListener: http.RequestListener = (req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></input></form></body>",
+    );
+    res.write("</head>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My Node App</title></head>");
   res.write("<body><h1>Hello, Node!</h1></body>");
   res.write("</head>");
-  res.end("Hello, Node!");
+  res.end();
 };
 
 const server = http.createServer((req, res) => {
