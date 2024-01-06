d82c476f62564b2f4cb8f5bd6c84a028f7da476f
Alberto Benatti <albi.benni8@gmail.com>
2024-01-06 19:28:41 +0100

    other

--- src/advanced-js/strings.ts
+++ src/advanced-js/strings.ts
@@ -7,6 +7,9 @@ log("\xA9"); // ©
 log("\u03c0"); // π
 log("\u{1f600}"); // 😀
 
+const love = "\ud83d\udc96";
+log(love, "length: ", love.length);
+
 const text = 'name = "John"';
 log(text);
 
@@ -15,6 +18,13 @@ const oneLineIn3Lines =
 one line \
 in three lines";
 log(oneLineIn3Lines);
+
+const threeLinesIn1Line = "This is \n 3 lines \n in one line";
+log(threeLinesIn1Line);
+
+const twoLinesBacktick = `This is
+two lines`;
+log(twoLinesBacktick);
 /**
  * String methods
  */
