c439a3a4dda8568e67f42795c6e2bed38d080c8b
Alberto Benatti <albi.benni8@gmail.com>
2024-01-06 19:15:38 +0100

    string props

---
+++ src/advanced-js/strings.ts
@@ -0,0 +1,20 @@
+import { log } from "console";
+
+/**
+ * Strings and string methods
+ */
+log("\xA9"); // ©
+log("\u03c0"); // π
+log("\u{1f600}"); // 😀
+
+const text = 'name = "John"';
+log(text);
+
+const oneLineIn3Lines =
+  "This is \
+one line \
+in three lines";
+log(oneLineIn3Lines);
+/**
+ * String methods
+ */
