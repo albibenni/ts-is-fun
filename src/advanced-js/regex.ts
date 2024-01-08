6ef187c257e74012ee49472da8b1e5eb34c13fe0
Alberto Benatti <albi.benni8@gmail.com>
2024-01-08 17:45:22 +0100

    reg

--- src/advanced-js/regex.ts
+++ src/advanced-js/regex.ts
@@ -1,10 +1,18 @@
-function extractNestedParenthesesContent(s: string): RegExpExecArray | [] {
-  const regExp = /\(([^)]+)\)/;
+import { log } from "console";
+
+function extractNestedParenthesesContent(s: string): RegExpExecArray | null {
+  const regExp = /\(([^)]+)\)/g;
   const matches = regExp.exec(s);
-  return matches ?? [];
+  return matches;
 }
 
-console.log(extractNestedParenthesesContent("((a), (b, (c, d)), (e))"));
-console.log(
-  extractNestedParenthesesContent("I expect five hundred dollars ($500)."),
-);
+log(extractNestedParenthesesContent("((a), (b, (c, d)), (e))"));
+log(extractNestedParenthesesContent("I expect five hundred dollars ($500)."));
+
+const extractNestedParenthesesContentOneLine = (
+  str: string,
+): RegExpMatchArray | null => {
+  return str.match(/\(([^)]+)\)/g);
+};
+
+log(extractNestedParenthesesContentOneLine("((a), (b, (c, d)), (e))"));
