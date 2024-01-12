b1aaaceee5915f8a7b78baf84d3264b79f56820f
Alberto Benatti <albi.benni8@gmail.com>
2024-01-12 17:36:27 +0100

    fix form action url

--- src/express-node/routes/admin.ts
+++ src/express-node/routes/admin.ts
@@ -4,11 +4,11 @@ const adminRoutes = Router();
 
 adminRoutes.get("/add-product", (req, res) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="product"><button type="submit">Send</button></form>',
+    '<form action="/admin/product" method="POST"><input type="text" name="product"><button type="submit">Send</button></form>',
   );
 });
 
-adminRoutes.post("/product", (req, res) => {
+adminRoutes.post("/add-product", (req, res) => {
   const prodBody = req.body.product;
   console.log(prodBody);
   res.redirect("/");
