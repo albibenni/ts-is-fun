6a4bd8320e6a2763db66c165c954ee965a449cae
Alberto Benatti <albi.benni8@gmail.com>
2024-01-12 17:33:34 +0100

    page not found middleware

--- src/express-node/app.ts
+++ src/express-node/app.ts
@@ -13,6 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/admin", adminRoutes);
 app.use("/shop", shopRoutes);
 
+//404
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
 // Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
