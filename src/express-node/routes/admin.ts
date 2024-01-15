60d5c8f3281842d8872bf32f095a47b13beecf1a
Alberto Benatti <albi.benni8@gmail.com>
2024-01-15 19:06:51 +0100

    fake cache

--- src/express-node/routes/admin.ts
+++ src/express-node/routes/admin.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import path from "node:path";
+export type Product = { title: string };
 
 const adminRoutes = Router();
+export const products: Product[] = [];
 
 adminRoutes.get("/add-product", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
 adminRoutes.post("/add-product", (req, res) => {
-  const prodBody = req.body.product;
-  console.log(prodBody);
-  res.redirect("/");
+  const prodBody = req.body.title;
+  products.push({ title: prodBody });
+  res.redirect("/shop");
 });
 
 export default adminRoutes;
--- src/express-node/routes/shop.ts
+++ src/express-node/routes/shop.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import path from "node:path";
+import { products } from "./admin";
 
 const shopRoutes = Router();
 
 shopRoutes.use("/", (req, res, next) => {
+  console.log(products);
   res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
 });
 
