cff72e44f68a41923bbcbdf8778fa44cc11d18b8
Alberto Benatti <albi.benni8@gmail.com>
2024-01-28 18:27:25 +0100

    get running processes

--- src/os-processes-terminal/test.ts
+++ src/os-processes-terminal/test.ts
@@ -18,3 +18,54 @@ const getCurrentFolderFileList = () => {
 };
 
 // getCurrentFolderFileList();
+
+const getRunningProcesses = () => {
+  exec("ps -e", (error, stdout, stderr) => {
+    if (error) {
+      log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      log(`stderr: ${stderr}`);
+      return;
+    }
+    // log(`stdout: ${stdout}`);
+    const lines = stdout.split("\n");
+    const processes = lines.filter((line) => line.includes("google"));
+    log(processes);
+    processes.forEach((process) => {
+      const processInfo = process.split(" ");
+      const pid = processInfo[0];
+      const command = processInfo[3];
+      log(`PID: ${pid} - Command: ${command}`);
+    });
+  });
+};
+
+const getRunningProcesses2 = (userID: string) => {
+  exec(`pgrep -l -u ${userID}`, (error, stdout, stderr) => {
+    if (error) {
+      log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      log(`stderr: ${stderr}`);
+      return;
+    }
+    // log(`stdout: ${stdout}`);
+    const lines = stdout.split("\n");
+    log(lines);
+    const processes = lines.filter((line) =>
+      line.toLowerCase().includes("discord"),
+    );
+    log(processes);
+    processes.forEach((process) => {
+      const processInfo = process.split(" ");
+      const pid = processInfo[0];
+      const command = processInfo[3];
+      log(`PID: ${pid} - Command: ${command}`);
+    });
+  });
+};
+
+getRunningProcesses2("benni");
