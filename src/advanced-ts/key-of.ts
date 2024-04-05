ba6b5e2368b8f9ad7775dbd2b38073a076e13d2d
Alberto Benatti <albi.benni8@gmail.com>
2024-04-05 22:24:29 +0200

    wip

--- src/advanced-ts/key-of.ts
+++ src/advanced-ts/key-of.ts
@@ -1,1 +1,12 @@
 console.log("keyof.ts");
+type Point = { x: number; y: number };
+type P = keyof Point;
+// ? keyof Point => 'x' | 'y'
+
+type Arrayish = { [n: number]: unknown };
+type A = keyof Arrayish;
+// ? number
+
+type Mapish = { [k: string]: boolean };
+type M = keyof Mapish;
+// ? string | number
