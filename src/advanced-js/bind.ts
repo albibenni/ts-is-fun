5044f475ad818de1d484cd64b25cba4673f26626
Alberto Benatti <albi.benni8@gmail.com>
2024-04-14 11:39:08 +0200

    bind

---
+++ src/advanced-js/bind.ts
@@ -0,0 +1,19 @@
+let c1 = {
+	x: 5,
+	y: 10
+}
+let c2 = {
+	x: 15,
+	y: 110
+}
+
+function printCoordinates (this: any) {
+	console.log(`${this.x}, ${this.y}`)
+}
+
+const c1_func = printCoordinates.bind(c1);
+c1_func(); // 5, 10
+
+
+const c2_func = printCoordinates.bind(c2);
+c2_func(); // 15, 110
