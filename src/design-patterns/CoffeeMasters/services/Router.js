5d211a5db50723c0d7ad7decc5cf20b4c3c6cde2
Alberto Benatti <albi.benni8@gmail.com>
2024-08-22 10:09:56 +0200

    lazy load stuff

--- src/design-patterns/CoffeeMasters/services/Router.js
+++ src/design-patterns/CoffeeMasters/services/Router.js
@@ -6,15 +6,15 @@ const Router = {
                 const href = event.target.getAttribute("href");
                 Router.go(href);
             });
-        });  
+        });
         // It listen for history changes
-        window.addEventListener('popstate',  event => {
+        window.addEventListener('popstate', event => {
             Router.go(event.state.route, false);
         });
-        // Process initial URL   
+        // Process initial URL
         Router.go(location.pathname);
-    },    
-    go: (route, addToHistory=true) => {
+    },
+    go: async (route, addToHistory = true) => {
         if (addToHistory) {
             history.pushState({ route }, '', route);
         }
@@ -24,18 +24,21 @@ const Router = {
                 pageElement = document.createElement("menu-page");
                 break;
             case "/order":
+                //lazy
+                await import("../components/OrderPage.js");
                 pageElement = document.createElement("order-page");
+                Router.setMetadata("Order", "blue");
                 break;
             default:
-                if (route.startsWith("/product-")) {                
+                if (route.startsWith("/product-")) {
                     pageElement = document.createElement("details-page");
-                    pageElement.dataset.productId = route.substring(route.lastIndexOf("-")+1);
+                    pageElement.dataset.productId = route.substring(route.lastIndexOf("-") + 1);
                 }
-                break;   
+                break;
         }
         if (pageElement) {
-            // get current page element            
-            let currentPage = document.querySelector("main").firstElementChild; 
+            // get current page element
+            let currentPage = document.querySelector("main").firstElementChild;
             if (currentPage) {
                 currentPage.remove();
                 document.querySelector("main").appendChild(pageElement);
