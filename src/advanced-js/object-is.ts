89f92b77a79a63fd59edce531b0549999009700a
Alberto Benatti <albi.benni8@gmail.com>
2024-09-01 21:22:19 +0200

    nice objects is overload

---
+++ src/advanced-js/object-is.ts
@@ -0,0 +1,36 @@
+if (!Object.is) {
+  Object.is = function ObjectIs(x: number, y: number) {
+    let xNegZero = isItNegZero(x);
+    let yNegZero = isItNegZero(y);
+
+    if (xNegZero || yNegZero) {
+      return xNegZero && yNegZero;
+    } else if (isItNaN(x) && isItNaN(y)) {
+      return true;
+    } else if (x === y) {
+      return true;
+    }
+    return false;
+
+    function isItNegZero(value: number) {
+      return value == 0 && 1 / value == -Infinity;
+    }
+
+    function isItNaN(value: number) {
+      return value !== value; // like NaN
+    }
+  };
+}
+
+console.log(Object.is(NaN, NaN) === true);
+console.log(Object.is(-0, -0) === true);
+console.log(Object.is(0, 0) === true);
+
+console.log(Object.is(-0, 0) === false);
+console.log(Object.is(0, -0) === false);
+console.log(Object.is(0, NaN) === false);
+console.log(Object.is(NaN, 0) === false);
+console.log(Object.is("42", 42) === false);
+console.log(Object.is(42, "42") === false);
+console.log(Object.is("foo", "bar") === false);
+console.log(Object.is(false, true) === false);
