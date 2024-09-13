ceae5bf71c8d2c24747db27e866d5bf89458919c
Alberto Benatti <albi.benni8@gmail.com>
2024-09-13 20:55:36 +0200

    closure example

--- src/advanced-js/closure-examples.js
+++ src/advanced-js/closure-examples.js
@@ -6,3 +6,30 @@ for (var i = 1; i <= 3; i++) {
 // i: 4
 // i: 4
 // i: 4
+//
+
+//************
+//example of fix
+//
+
+for (var i = 1; i <= 3; i++) {
+  let j = i;
+  setTimeout(function () {
+    console.log(`j: ${j}`);
+  }, i * 1000);
+}
+// j: 1
+// j: 2
+// j: 3
+
+// or
+//
+
+for (let i = 1; i <= 3; i++) {
+  setTimeout(function () {
+    console.log(`i: ${i}`);
+  }, i * 1000);
+}
+// i: 1
+// i: 2
+// i: 3
