420cfedd95c77f31db210c12abe6fc3f00453294
Alberto Benatti <albi.benni8@gmail.com>
2024-10-18 11:09:37 +0200

    increment with closure variable

--- src/advanced-js/closure2.ts
+++ src/advanced-js/closure2.ts
@@ -8,3 +8,17 @@ function createFunction() {
 const generatedFunc = createFunction();
 const result = generatedFunc(3);
 console.log(result);
+
+function outer() {
+  let counter = 0;
+  function increment() {
+    counter++;
+    console.log(counter);
+  }
+  return increment;
+}
+
+const myFunc = outer();
+myFunc();
+myFunc();
+myFunc();
