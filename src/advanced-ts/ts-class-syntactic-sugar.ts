1b042fd86aac99fa9003de37f59c537a4af40b6b
Alberto Benatti <albi.benni8@gmail.com>
2024-11-02 17:02:37 +0100

    class constructore syntactic sugar

---
+++ src/advanced-ts/ts-class-syntactic-sugar.ts
@@ -0,0 +1,8 @@
+class Car {
+  constructor(public make: string, public model: string, public year: number) {}
+    // like this.make = make
+}
+
+const myCar = new Car("Honda", "Accord", 2017);
+myCar.make;
+//     ^|
