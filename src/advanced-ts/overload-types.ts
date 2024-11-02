f58234945ff50426b4fff2aa48084218c68e8598
Alberto Benatti <albi.benni8@gmail.com>
2024-11-02 14:14:11 +0100

    overload

---
+++ src/advanced-ts/overload-types.ts
@@ -0,0 +1,21 @@
+type FormSubmitHandler = (data: FormData) => void;
+type MessageHandler = (evt: MessageEvent) => void;
+
+function handleMainEvent(
+  elem: HTMLFormElement,
+  handler: FormSubmitHandler
+): void;
+
+function handleMainEvent(
+  elem: HTMLIFrameElement,
+  handler: MessageHandler
+): void;
+
+function handleMainEvent(
+  elem: HTMLFormElement | HTMLIFrameElement,
+  handler: FormSubmitHandler | MessageHandler
+) {
+  console.log(elem, handler);
+}
+
+// overload
