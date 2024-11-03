5c15336fe25bd3a7e0d8fe936cd81cbe1279efc2
Alberto Benatti <albi.benni8@gmail.com>
2024-11-03 10:24:29 +0100

    exe

---
+++ src/advanced-ts/generic-exercise/exercise.ts
@@ -0,0 +1,110 @@
+///// SAMPLE DATA FOR YOUR EXPERIMENTATION PLEASURE (do not modify)
+// https://www.typescriptlang.org/play/?#code/PQ18ZXgUNIAIAqBRAykgkgOQOIIFUsAZTLdBeYOSBAOwHt6BTZgEwQBcmBbBtgJYAzAJ5cAFswDOzSpBpVFS0DQQABZgCdNDTVIBcCAOwAGEwDZjZy6YtW7t8zADGDOlIYAbZgDpn3gENNAAoAShgYIQBXOmdOATcEAKkZTU4UAEcogM8pAB4kAD5gmAQEIQYYtkMkABpShGYADwAHZjj2GvqynmkpAIBzZkMpTk0BOgGYUIQAbwbhBGCKqoQAQgBeDcbW9s52cLKyznEdAHcWC5RtXRKjo4ADQBlyBABBFK14xKEAgW9qhAAElmvRSg2YAF8ADp0ZptDoA4FwvbsaF0FZ0RGzDFsCEPBplQ4IVzuLy+TwMAbBB6AUHIEAB5ADSQJBfXBePCEIi0ViXzoSQ+aXpAGtggA3HJRYZJOgiWoIUH9IYjMYTAYzeZlRbBNYSzxSonHU4MC6wq43ELPN6CvnlX7-QzAxXsh5Ekkebw+ClU2kM5lOtlDDkwLmgBAAWkjziinEj4Yi4AQaFeAFkAArEFAIAAiryQrwQADF6QAlBAATXpBDLKAAGmmUCXMCmUNh81h6dgEBmUK80NWs8E2ExGJwFfxhCJwu6x0JNFEBJwpAhtpqki0Wt5DLNiV5dIYAESadgH+U8ZIGBAARjMCAh3QQA00ATa293FM0h+PbFPCovhgAVjvB8ACMAjocCAjfVwP0PERmE8Ckzl-c8UkMK8AA4AGZgIabw+DoaC90-BAD3gxCTRQ-8EAwkxcLKNogiI2DSKfVg6CotDryMDD6IQXRwOVOZ333UiBMmZhOMvAAmcxpL458pBaECtE0ERmNEo8TzPaiABY+OcSRtHU4SYM078pMA3CuRgCZ9k0H5nFkbMBDiApCjmBoAG1hRVcZJgAXRqEME2AN5tACEQfBaHRuE4EQ2h8c8WnlECY3KXQc1czhIhiOIEn5ZKXLiYIfDKoIBkvcCRC8gLQkMaq5lDMLXgiqKYoYOKEt8IQ-ns1L0oqTQsriXLeQK8o+q0YrOFK8rNEqhrZVq+qZTEWZmvC592tizrup8b8oicgbZ0ymaxvyxJDqcma5p8CqquWuqlvWmzlHexQFHgZB0CQJMCDILN3q+sB6BHVgOG4cdBFECRpFkKAQY+5GVFUFNXjTFw3FGcp50XKQAHVF3ERkBhTC8Vz-FpbrnBcl3lZY8c4eUIN6GYNg84I1zKnxacXB9hQGQwTB8MwrwQAAqXG6aSi8H1Z5h6ghUJp2xsd8LcbAAl6Py1UpvmlyJk5SfJlIvWYAifAV1Q1CXcMBAGRhj3DVTdCx9xZ3tYgLc17XpToKIeBU4btgNwniZNi9zct62L0+EU7ml-GjZJsm5YaA8vPRtMAqpmaEGPTgok0dwEA8XoTj1sYY3EEQD2mGA47STJslyROw5TyOzcM1SooVh8Dx74zTwaAByLOMdzvwjLUhBxGSJJSIVg8EA6to0jEM5iYQPUpVIoe1IPUeG6b9IshyKR26Z8PjbT7uZ6iwWHxFswAIfcfs6ng+xHn5cAlIwWK816fE3tvXesgX4mAAsfcIp8W4XyvnTG+qdTZSGnr3WWKQHxvzHhPHOCB0HGTngvf+B5UJSCATode8UEBbxODvSUshoEnxtPAtuDR6QgQAFZ7B8MKZgIhL4dwjnfKQoRzaTBOA+DCA88G52EbfVBZdxCVE8BweeYpZC8X4YI+u4RVCFkwMQVAJZVBuE8GIb8Sclzuxxt+Qs199ZTU0LdBoYcHyMzpuzDyBs-DERXFsUiFkWGpE4AnBo9jr6yMMcYxs8jnH50LsXUu5dmCV0mFwecJw64hM+Gwy+HDuG8J0ZfSJSDxHeEkeIB8ulolGJMbnfSYcJABDHP0eIUhYYnFkL1Tw9k9GN1YefdhZROE8LiHwgRpT2AOPKT4DwaQwg+C4QwCYwRfwHlCAPF8m5FbEgfvKJ8L49mKWUr3EeZRx4xIaQgRkUykjHlIjs7w8pv6HOfG0eUpzg6HxgaFBAJYUDZgIAAYRQKoTAQhaGyHnhwNwsgGBQuYAEQy1j5QqIuDwI64haGqMhpILgnUch-hSAgFSAB+WxY54X0iECgFF4hZmLiUdsa6zBXFlHcQ0YI0ZtDMDoJwVB8oDbeOJMXY8AqlEAGprGYKkM-XJQpRQRJmVEjOXlAXArBbnNliS0nJOXKk9JAxMk1xybAoZrcCnHG6oi-isJaX0sMkywV6cygHgDkHLQFzSIaqBaClAOr2BHXZdlAu+qS5-3oIHYOAy4HDOtfa5gjqGUuqFQ0DCV4jCyM1QG3OmajAmvIQgRYZwYUBDhbCfiSKGXWL0UAA
+const fruits = {
+  apple: { color: "red", mass: 100 },
+  grape: { color: "red", mass: 5 },
+  banana: { color: "yellow", mass: 183 },
+  lemon: { color: "yellow", mass: 80 },
+  pear: { color: "green", mass: 178 },
+  orange: { color: "orange", mass: 262 },
+  raspberry: { color: "red", mass: 4 },
+  cherry: { color: "red", mass: 5 },
+}
+
+interface Dict<T> {
+  [k: string]: T
+}
+
+// Array.prototype.map, but for Dict
+function mapDict(...args: any[]): any {}
+// Array.prototype.filter, but for Dict
+function filterDict(...args: any[]): any {}
+// Array.prototype.reduce, but for Dict
+function reduceDict(...args: any[]): any {}
+
+/////////////////////////////////////////
+///////////// TEST SUITE ///////////////
+//////// no need to modify these ////////
+/////////////////////////////////////////
+
+// MAP
+const fruitsWithKgMass = mapDict(fruits, (fruit, name) => ({
+Parameter 'fruit' implicitly has an 'any' type.
+Parameter 'name' implicitly has an 'any' type.
+  ...fruit,
+  kg: 0.001 * fruit.mass,
+  name,
+}))
+const lemonName: string = fruitsWithKgMass.lemon.name
+// @ts-ignore-error
+const failLemonName: number = fruitsWithKgMass.lemon.name
+assertOk(
+  fruitsWithKgMass,
+  "[MAP] mapDict returns something truthy"
+)
+assertEquals(
+  fruitsWithKgMass.cherry.name,
+  "cherry",
+  '[MAP] .cherry has a "name" property with value "cherry"'
+)
+assertEquals(
+  fruitsWithKgMass.cherry.kg,
+  0.005,
+  '[MAP] .cherry has a "kg" property with value 0.005'
+)
+assertEquals(
+  fruitsWithKgMass.cherry.mass,
+  5,
+  '[MAP] .cherry has a "mass" property with value 5'
+)
+assertEquals(
+  Object.keys(fruitsWithKgMass).length,
+  8,
+  "[MAP] fruitsWithKgMass should have 8 keys"
+)
+
+// FILTER
+// only red fruits
+const redFruits = filterDict(
+  fruits,
+  (fruit) => fruit.color === "red"
+Parameter 'fruit' implicitly has an 'any' type.
+)
+assertOk(
+  redFruits,
+  "[FILTER] filterDict returns something truthy"
+)
+assertEquals(
+  Object.keys(redFruits).length,
+  4,
+  "[FILTER] 4 fruits that satisfy the filter"
+)
+assertEquals(
+  Object.keys(redFruits).sort().join(", "),
+  "apple, cherry, grape, raspberry",
+  '[FILTER] Keys are "apple, cherry, grape, raspberry"'
+)
+
+// REDUCE
+// If we had one of each fruit, how much would the total mass be?
+const oneOfEachFruitMass = reduceDict(
+  fruits,
+  (currentMass, fruit) => currentMass + fruit.mass,
+Parameter 'currentMass' implicitly has an 'any' type.
+Parameter 'fruit' implicitly has an 'any' type.
+  0
+)
+assertOk(
+  redFruits,
+  "[REDUCE] reduceDict returns something truthy"
+)
+assertEquals(
+  typeof oneOfEachFruitMass,
+  "number",
+  "[REDUCE] reduceDict returns a number"
+)
+assertEquals(
+  oneOfEachFruitMass,
+  817,
+  "[REDUCE] 817g mass if we had one of each fruit"
+)
---
+++ src/advanced-ts/generic-exercise/solution.ts
@@ -0,0 +1,126 @@
+///// SAMPLE DATA FOR YOUR EXPERIMENTATION PLEASURE (do not modify)
+// ******* playground
+// https://typescriptlang.org/play/?#code/PTAEAEGcBcCcEsDG0Bco4FcCmAoE+DCjiSi9SwAVAUQGVKBJAOQHFQBVRgGQcbtArByRUADsA9mKxYAJukkBbcTPgAzAJ7oAFlkhYBxYYOMmQORONGRxAGywA6RHYCGsABQBKHDlUZRyeEtQZ0g9WGhqAEcMZxtIAB5KAD43HFBQVXE-GTRKABo00CwADwAHLGRZXIL0hV1IZwBzLDQYBFFGnA9QAG9CtVA3TOzQAEIAXnGisoroWS909OgtWHEAdykN6lhV90LF0AADQBlyUABBUKxwwNEM53g7HNAAEh660KasAF8AHVES8qVJ6vAGzWS-UTDUTAnpQmRfQ77UALUAWKy2Bw2cSNNyHQCg5KAAPIAaReb3qnwRXi+3l8-mgN2Cl3ChIA1m4AG6xbBoZyidR5UDvBrNVpweAdbp9dIDNyjLk2bAopYrdabUDbXa404XMIMoKqe6PNCvYWUw4otHWOz2LE4-FE0mminNKk4Gn4UAAWh9iAw0B9Xu8hFAtDOAFkAApcaigAAiZ0oZ1AADFCQAlUAATUJ7Ez1AAGpHqOmGOHqEwk4xCUxQNHqGdaHnY24ZJIJNAhco1OovFbO6pYBh4NBIKAptLgqVSnY0D1UbZxLA0AAiWCyFeChQhSBoACMAAYD6AvjVQI1YM5ynOF1jl6A1xutzu0ABWE9ngBGfJ-N4sd9XdQsBsLE1k3IUX1APcAA4AGYP0KOwlFEP9F3vFcgJA9ZwO3UI0Gg49T0KcpXFQgCHwvaRRBwyC9wAdmghD0iXPlRV6W8l1XFiOiwGi8NAAAmAA2ASmNAS9IFKT8rlgdQyM4h91xkPjd1AAAWMTEB0HY5PY-8FMfZTn3498iJpHAJTmWBDUQfQ4yQaBEiSXpCgAbVZMV2kaABdXJ3WDMAzh2Zx1HsUpVmgcRoHUcp7G3UpBU-f0MiXeMHJ8PwAiCeL7OQRJBVoFJ+lEUp-Vy1A0ry5Iz3ilNMv1FDBiXeBGglWJqlAVksF0toJUabpxmc2gujQcr4kKlz0n7UAsmgcrRoc8bnInGl0kyWBBjsTtWRm1QiU-AArWZ7C69RIDcCVSrmhyPClJFpuWeBIAANVicdQEusqHPc7ykVm8qfve2r6puNxHpe2JBVZFFVvErBoAwWBbn+9KPUC4LQvCqKopihxVAeKzEuS9bKugDL6UZfGbCssbklSGUSq+5AFqqpIzypqy6opyw0E5bkWlAfJOu6zy+oG5zP3EDE+RG0mnMmhcrE7FHmbl5J3p6WGSbcLbOt2-ajuQE7uvOz7ruQW6FamywYG0J7Xpsd6zYB1lfoOWUOauLmstEMGtHtyHOstlXoEBqZwYdwpYfXBGkZmpmybR84MbCiKcdipSMFsomB1S8ryZ9uGZCzrBaYKoqGau+a1bZwpM9s2BvYa3n7sR9dRGgB20FoM90hUZAGDmBRqiRU7RY6QpxdDM8JRHeBYgdnlQ1l2gFd1hVsCd0Q54X-nCm13WdvEPbCUO47TtNxnzegS3J2tpW7Yhx2pmd77XaRDf9CmeuvZByw+cVFgQUEdA7QyjnXeGiNbif38kYUw8CSBwJIILOglBQzsF4LGeBSCQBiHbNIOQkUuwqA0NoXQ+hEEIKoYg8goBwxnEjOYG2A4hwjkgAAdRHFoYkjRww7iBlecqQxWGjkFMI4c0BBSiGcHUKebhJz2EUYOCRZ5WSNDQAeewR49ygAAFQZBEXFHcZ5pF1AKF8W6TCH5IUsEwGRAteodHesothnDlg8L4aEW0WBkL2FMbgXB4BRxehahIdcXoZJLisbbQ0DwuA+NsfYtAogMAKGkhtKYLjRxuO4bwnc3jfH+JwDuK40A2T0wMRIjhXCPHGMKCuVy9DIzeQgqUcqcNY5WFANYOoj0nGYGWOoFcXRinMgiNEWI5194iOqe4vJXitIyVCv4s8K5Fk6U3IUAA5I0hhLTHDaVkqALQIRggPn8SuUAWNyjhE0GsLhoBP4PnWbJFcWyRklPCFEGIcQKlZNmbkzxkADlLJOo0M8mijyvjPDspp+yXmaBOWOZwD41GXOuaUu5DynmQoPK+d5XhPnjJ+VMtaMycm1IWYc0KuFIBnmhds3ZzTQAgp0sc05KKVy0vRasG50VQD3OWI8-moB8UfLGd8yZFTT6G2gMbM64jXE1PmZADw3iOjLDPNBVZTKWn-IpSq7pWgsg2DkCcjk+hGIX2GV4WhKYGBcBoOmWhlgbCaCUpUth0TOxKRTDM5xBMrhCOmVUs8iqb7jmclkxwaFxyTEUhucVepykQJkH60N9TXL2sdSWPVgbYDtJjlAscPT4b+36UOQZNrRl6klb8woMrz4mzcL6mZaq7Aaq0GeNSOrs1OpaRpf52hnCdgaAySApDlj6E9rAatRK62koNk2hVraqlqusOETw9gDriAlG4cCK4PCrKvDOIBqJqWCgvFeM9EkpJLM2ekHZfbc2gGJCbYI64HwnrsIKBFl7LzlEFLe9JryCUBVAOmagcZ2AAGFqC0IYHtNY+gTlyEsPoY+RRnBaU9ZI45aoFBZy0AKk1hCdDyGgG9WloBpIAH5vUzX+ISVQ1BsNaHTSOIF70f7BrJRm9Ibg-Q7CwB3IFgoslTyE+3aAXGADUuGjGhAhUm0pKb0irrYTqyD0G4MtJ4w5DpxbuniF6eWxo6BK1aCGSpr5Ez61LFxph9DzHWNaQ4zJup6QVwpLSVcB9D5XLadg9QPTsgS6FsgUjZFYhUnpLnRKuzi7nMsbY+5sThRoL0S01B4LLTMt0XM9RgYyH2Vof+PrLAbHcM2qAA
+
+const fruits = {
+  apple: { color: "red", mass: 100 },
+  grape: { color: "red", mass: 5 },
+  banana: { color: "yellow", mass: 183 },
+  lemon: { color: "yellow", mass: 80 },
+  pear: { color: "green", mass: 178 },
+  orange: { color: "orange", mass: 262 },
+  raspberry: { color: "red", mass: 4 },
+  cherry: { color: "red", mass: 5 },
+};
+
+interface Dict<T> {
+  [k: string]: T;
+}
+
+// Array.prototype.map, but for Dict
+function mapDict<T, S>(
+  inputDict: Dict<T>,
+  mapFunction: (original: T, key: string) => S,
+): Dict<S> {
+  const outDict: Dict<S> = {};
+  for (let k of Object.keys(inputDict)) {
+    const thisVal = inputDict[k];
+    outDict[k] = mapFunction(thisVal, k);
+  }
+  return outDict;
+}
+// Array.prototype.filter, but for Dict
+function filterDict<T>(
+  inputDict: Dict<T>,
+  filterFunction: (value: T, key: string) => boolean,
+): Dict<T> {
+  const outDict: Dict<T> = {};
+  for (let k of Object.keys(inputDict)) {
+    const thisVal = inputDict[k];
+    if (filterFunction(thisVal, k)) outDict[k] = thisVal;
+  }
+  return outDict;
+}
+// Array.prototype.reduce, but for Dict
+function reduceDict<T, S>(
+  inputDict: Dict<T>,
+  reducerFunction: (currentVal: S, dictItem: T, key: string) => S,
+  initialValue: S,
+): S {
+  let value = initialValue;
+  for (let k of Object.keys(inputDict)) {
+    const thisVal = inputDict[k];
+    value = reducerFunction(value, thisVal, k);
+  }
+  return value;
+}
+
+/////////////////////////////////////////
+///////////// TEST SUITE ///////////////
+//////// no need to modify these ////////
+/////////////////////////////////////////
+
+// MAP
+const fruitsWithKgMass = mapDict(fruits, (fruit, name) => ({
+  ...fruit,
+  kg: 0.001 * fruit.mass,
+  name,
+}));
+const lemonName: string = fruitsWithKgMass.lemon.name;
+// @ts-ignore-error
+const failLemonName: number = fruitsWithKgMass.lemon.name;
+assertOk(fruitsWithKgMass, "[MAP] mapDict returns something truthy");
+assertEquals(
+  fruitsWithKgMass.cherry.name,
+  "cherry",
+  '[MAP] .cherry has a "name" property with value "cherry"',
+);
+assertEquals(
+  fruitsWithKgMass.cherry.kg,
+  0.005,
+  '[MAP] .cherry has a "kg" property with value 0.005',
+);
+assertEquals(
+  fruitsWithKgMass.cherry.mass,
+  5,
+  '[MAP] .cherry has a "mass" property with value 5',
+);
+assertEquals(
+  Object.keys(fruitsWithKgMass).length,
+  8,
+  "[MAP] fruitsWithKgMass should have 8 keys",
+);
+
+// FILTER
+// only red fruits
+const redFruits = filterDict(fruits, (fruit) => fruit.color === "red");
+assertOk(redFruits, "[FILTER] filterDict returns something truthy");
+assertEquals(
+  Object.keys(redFruits).length,
+  4,
+  "[FILTER] 4 fruits that satisfy the filter",
+);
+assertEquals(
+  Object.keys(redFruits).sort().join(", "),
+  "apple, cherry, grape, raspberry",
+  '[FILTER] Keys are "apple, cherry, grape, raspberry"',
+);
+
+// REDUCE
+// If we had one of each fruit, how much would the total mass be?
+const oneOfEachFruitMass = reduceDict(
+  fruits,
+  (currentMass, fruit) => currentMass + fruit.mass,
+  0,
+);
+assertOk(redFruits, "[REDUCE] reduceDict returns something truthy");
+assertEquals(
+  typeof oneOfEachFruitMass,
+  "number",
+  "[REDUCE] reduceDict returns a number",
+);
+assertEquals(
+  oneOfEachFruitMass,
+  817,
+  "[REDUCE] 817g mass if we had one of each fruit",
+);
