060a770622f84f78d69e3c04af26616c8f1bc612
Alberto Benatti <albi.benni8@gmail.com>
2024-11-03 10:21:45 +0100

    guards

---
+++ src/advanced-ts/type-guard-carlike.ts
@@ -0,0 +1,42 @@
+//* User-defined type guards
+
+interface CarLike {
+  make: string;
+  model: string;
+  year: number;
+}
+
+// the guard
+function isCarLike(valueToTest: any): valueToTest is CarLike {
+  return (
+    valueToTest &&
+    typeof valueToTest === "object" &&
+    "make" in valueToTest &&
+    typeof valueToTest["make"] === "string" &&
+    "model" in valueToTest &&
+    typeof valueToTest["model"] === "string" &&
+    "year" in valueToTest &&
+    typeof valueToTest["year"] === "number"
+  );
+}
+
+function assertIsCarLike(valueToTest: any): asserts valueToTest is CarLike {
+  if (
+    !(
+      valueToTest &&
+      typeof valueToTest === "object" &&
+      "make" in valueToTest &&
+      typeof valueToTest["make"] === "string" &&
+      "model" in valueToTest &&
+      typeof valueToTest["model"] === "string" &&
+      "year" in valueToTest &&
+      typeof valueToTest["year"] === "number"
+    )
+  ) {
+    throw new Error(`Value does not appear to be a CarLike${valueToTest}`);
+  }
+}
+
+const car = {};
+isCarLike(car);
+assertIsCarLike(car);
