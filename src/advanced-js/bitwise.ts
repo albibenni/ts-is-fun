acb6ae044849a60c62fdbdb32441ef9988953a21
Alberto Benatti <albi.benni8@gmail.com>
2026-01-01 10:38:14 +0100

    other way for unsigned int

--- src/advanced-js/bitwise.ts
+++ src/advanced-js/bitwise.ts
@@ -7,5 +7,16 @@ console.log((a << 1) >> 1);
 console.log((a << 2) >> 1);
 
 console.log(-100000000000 >>> 1);
+
 //NOTE: to convert negative number to unsigned int
 console.log(-1 >>> 0);
+
+//NOTE: using typed array to store unsigned int
+const t: Uint32Array = new Uint32Array(1);
+t[0] = -1;
+console.log(t[0]);
+
+//NOTE: using Buffer to store unsigned int
+const buf = Buffer.alloc(4);
+buf.writeUInt32BE(4294967295, 0);
+console.log(buf.readUInt32BE(0));
